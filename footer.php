80d355178a1f00d41ae8b68d11dbf649d6009d8c
pradnya15 <shirke.pradnya201@gmail.com>
2020-05-07 16:16:39 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+
+    <!-- Rodapé -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Mastertech 2017</p>
+        </div>
+    </footer>
+    <!-- /Rodapé -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Importa o Javascript utilizado neste projeto -->
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Shop Homepage - Mastertech Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Importa o CSS utilizado neste projeto -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
